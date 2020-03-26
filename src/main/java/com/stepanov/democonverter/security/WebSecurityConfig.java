f747d22563f03aa426d3e48b6c6f909582a0357c
Max Stepanov <mu5cool@yandex.ru>
2020-03-26 10:11:08 +0300

    Switch on BcryptPasswordEncoder

--- src/main/java/com/stepanov/democonverter/security/WebSecurityConfig.java
+++ src/main/java/com/stepanov/democonverter/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.stepanov.democonverter.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,21 +9,17 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("userDetailsService")
+    private final UserDetailsService userDetailsService;
     @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public WebSecurityConfig(@Qualifier("userDetailsService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -50,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO USER (id, login,password) VALUES
-(1, 'aaa', '111'),
-(2, 'bbb', '222'),
-(3, 'ccc', '333');
+(1, 'aaa', '$2y$12$nl.csFj0s4GTv7BudYMkwumk8mVsahtkdIs9O00PHP2DxijsQdfIy'),
+(2, 'bbb', '$2y$12$eenfbfTGR02S0ZNx/xV.GuVInPJScZmJiR7vrL6nVkDKyHKF.1AXq'),
+(3, 'ccc', '$2y$12$gKKQFXbJ0m8eBacDM0fltuoEQleFahoiGuiQKfdyETAJNto87wmce');
 
